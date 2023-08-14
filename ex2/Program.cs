2c8a3d8ce48b65024f308637f7b97132138160b2
lilYummy228 <142146352+lilYummy228@users.noreply.github.com>
2023-08-14 20:29:26 +0500

    Добавьте файлы проекта.

---
+++ ex2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int money;
+            float metersInMillimeters;
+            double piNumber;
+            string greetingsText;
+            char symbol;
+            bool isOpen;
+            long lightSpeed;
+            ushort heightInMeters;
+            ulong population;
+            decimal divisionResult;
+        }
+    }
+}
